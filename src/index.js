a5ddf13902fd2c54be9fe0a9d2345cd550409d63
AlenaVP <poprugo@gmail.com>
2021-03-13 15:22:47 +0300

    feat: add a solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let next = 1;
+
+  while (next > 0) {
+    next = 0;
+    for (let array of bracketsConfig) {
+      let cpl = (array[0] + array[1]);
+      while(str.includes(cpl)) {
+        str = str.replace(cpl, '');
+        next++;
+      }
+    }
+  }
+
+  return str.length === 0;
 }
